b559b76470497fe28b87b017b89ca64b64be0294
Aakansha5Gupta <aakanshag87@gmail.com>
2025-11-12 14:28:29 +0530

    version 1.1

--- First_cmd.c
+++ First_cmd.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main () 
 {
-    printf("Hello World");
+    printf("Hello Aakansha");
     printf("Name:Aakansha Gupta\n");
     printf("Age:19\n");
     printf("University:Shobhit University\n");
