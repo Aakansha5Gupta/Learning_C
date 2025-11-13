1e1816b96ce41bdb34971c14aa06102b3e1cd93b
Aakansha5Gupta <aakanshag87@gmail.com>
2025-11-13 08:05:06 +0530

    untitled-1.c

---
+++ Untitled-1.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+printf("HELLO WORLD");
+printf("My name is Aakansha Gupta.");
+printf("I am currently pursuing BCA with specialization in Cyber Security from Shobhit University.");
+printf("I am in my first year right now.");
+printf("I choose this course because I am interested in computers technologies, and cyber security.");
+printf("My goal is become a succesful cyber security expert and also prepare for government job in the future.");
+printf("Apart from studies,I like reading,learning new things,and helping others.");
+printf("Thanks you");
+return 0;
+}
