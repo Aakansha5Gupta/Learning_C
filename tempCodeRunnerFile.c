4acf6df3dc69677060e50546acbb38cff025bb78
Aakansha5Gupta <aakanshag87@gmail.com>
2025-11-14 18:23:41 +0530

    first commit

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int n1,n2,s;
+    n1=10;n2=14;
+    printf("enter n1 : %d\n",n1);
+    printf("enter n2:%d\n",n2);
+    s = n1+n2;
+    printf("sum of n1 & n2 :%d\n",s);
+return 0;
+}   
