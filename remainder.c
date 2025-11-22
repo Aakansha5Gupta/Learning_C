42771a6234ade94564851dc0ee12a3c0dc4facfa
aakansha5gupta <aakanshag87@gmail.com>
2025-11-22 19:46:50 +0530

    FIRST COMMIT

---
+++ remainder.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n1,n2,R;
+    n1=2;n2=5;
+    printf("enter n1:%d\n",n1);
+    printf("enter n2:%d\n",n2);
+    R=n1%n2;
+    printf("remainder of n1 & n2 : %d",R);
+    return 0 ;
+}
---
+++ sum_of_two.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1, n2, s;
+
+    n1 = 10;
+    n2 = 20;
+
+    printf("n1 = %d\n", n1);
+    printf("n2 = %d\n", n2);
+
+    s = n1 + n2;
+
+    printf("Sum = %d\n", s);
+
+    return 0;
+}
